ef852f8ba469b07a86413a5f983dcc0d7c4e7262
Cyrus Choi <77631273+cyruschoisy@users.noreply.github.com>
2022-09-30 14:46:48 -0400

    Update Assignment_1A.java

--- Unit 1 - Recursion/Assignment_1A.java
+++ Unit 1 - Recursion/Assignment_1A.java
@@ -5,7 +5,7 @@
 
 import java.io.*;
 import java.util.*;
-public class PartA {
+public class Assignment_1A {
     public static void main(String[] args) {
         boolean isValidAmount = false;
         double total = 0;
